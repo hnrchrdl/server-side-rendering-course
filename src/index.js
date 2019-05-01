075ba29d97730e3b67c2dd9cc79ee7c901651ed9
hnrchrdl <hinrich@posteo.de>
2019-05-01 13:53:42 +0200

    2-13

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const express = 'express';
+const express = require('express');
 const React = require('react');
 const renderToString = require('react-dom/server').renderToString;
 const Home = require('./client/components/Home').default;
 const app = express();
 
 app.get('/', (req, res) => {
-    const content = renderToString(<Home />>);
+    const content = renderToString(<Home />);
     res.send(content)
 })
 
