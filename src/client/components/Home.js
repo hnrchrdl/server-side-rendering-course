9fd0fed1d89a2e8ee32495d3894620813011fd26
hnrchrdl <hinrich@posteo.de>
2019-05-01 16:12:04 +0200

    3-17

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-    return <div>I'm the Home Component</div>;
+  return (
+    <div>
+      <div>I'm the Home Component</div>
+      <button
+        onClick={() => {
+          console.log("click");
+        }}
+      >
+        click me
+      </button>
+    </div>
+  );
 };
 
 export default Home;
