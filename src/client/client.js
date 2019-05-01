22c388ad80cc9e15ff25121d69811962b52d4094
hnrchrdl <hinrich@posteo.de>
2019-05-01 17:35:39 +0200

    5-28

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,12 @@
 // startup point for the client side application
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Home from './components/Home'
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./Routes";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.hydrate(<Home />, document.querySelector('#root'))
+ReactDOM.hydrate(
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
+  document.querySelector("#root")
+);
