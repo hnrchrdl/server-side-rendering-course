1374de9562efb4cc19bc4403551eb5f66937e426
hnrchrdl <hinrich@posteo.de>
2019-05-01 17:46:53 +0200

    5-30

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import { renderToString } from 'react-dom/server'
-import Home from '../client/components/Home'
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { StaticRouter } from "react-router-dom";
+import Routes from "../client/Routes";
 
-export default () => `<html>
+export default (req) => {
+  const content = renderToString(
+    <StaticRouter location={req.path} context={{}}>
+      <Routes />
+    </StaticRouter>
+  );
+  return `<html>
     <head>
     </head>
     <body>
-        <div id="root">${renderToString(<Home />)}</div>
+        <div id="root">${content}</div>
         <script src="bundle.js"></script>
     </body>
-</html>`
+</html>`;
+};
