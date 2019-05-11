bce2371ea598d64ede1d72a431c2f5b556a7e889
hnrchrdl <hinrich@posteo.de>
2019-05-11 16:29:51 +0200

    7-45

--- src/client/client.js
+++ src/client/client.js
@@ -3,6 +3,7 @@ import 'babel-polyfill'
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { renderRoutes } from 'react-router-config'
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
@@ -14,7 +15,7 @@ const store = createStore(reducers, {}, applyMiddleware(thunk));
 ReactDOM.hydrate(
   <Provider store={store}>
     <BrowserRouter>
-      <Routes />
+      <div>{renderRoutes(Routes)}</div>
     </BrowserRouter>
   </Provider>,
   document.querySelector("#root")
--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom";
+import { renderRoutes } from "react-router-config";
 import { Provider } from "react-redux";
 import Routes from "../client/Routes";
 
 export default (req, store) => {
-
-    const content = renderToString(
-        <Provider store={store}>
-        <StaticRouter location={req.path} context={{}}>
-            <Routes />
-        </StaticRouter>
-        </Provider>
+  const content = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.path} context={{}}>
+        <div>{renderRoutes(Routes)}</div>
+      </StaticRouter>
+    </Provider>
   );
 
-  console.log(content)
+  console.log(content);
 
   return `<html>
     <head></head>
