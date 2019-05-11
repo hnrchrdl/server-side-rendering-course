d97785ddf8c32f78737948ab18b2bb9a763ecea8
hnrchrdl <hinrich@posteo.de>
2019-05-11 16:33:43 +0200

    7-46

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import 'babel-polyfill'
+import "babel-polyfill";
 import express from "express";
+import { matchRoutes } from "react-router-config";
+import Routes from "./client/Routes";
 import renderer from "./helpers/renderer";
 import createStore from "./helpers/createStore";
 
@@ -9,12 +11,10 @@ app.use(express.static("public"));
 
 app.get("*", (req, res) => {
   const store = createStore();
-
-  // bla
-
+  const path = req.path;
+  console.log(matchRoutes(Routes, path))
   return res.send(renderer(req, store));
 });
-
 
 app.listen(3000, () => {
   console.log("listening on 3000");
