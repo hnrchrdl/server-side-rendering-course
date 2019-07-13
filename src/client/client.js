0bb5bc34d98031676dcca9c88ffaa8dfde2bd143
hnrchrdl <hinrich@posteo.de>
2019-07-13 15:49:07 +0200

    8-56

--- src/client/client.js
+++ src/client/client.js
@@ -1,16 +1,20 @@
 // startup point for the client side application
-import 'babel-polyfill'
+import "babel-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { renderRoutes } from 'react-router-config'
+import { renderRoutes } from "react-router-config";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import Routes from "./Routes";
-import reducers from './reducers'
+import reducers from "./reducers";
 
-const store = createStore(reducers, {}, applyMiddleware(thunk));
+const store = createStore(
+  reducers,
+  window.INITIAL_STORE,
+  applyMiddleware(thunk)
+);
 
 ReactDOM.hydrate(
   <Provider store={store}>
--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -20,6 +20,9 @@ export default (req, store) => {
     <head></head>
     <body>
         <div id="root">${content}</div>
+        <script>
+          window.INITIAL_STORE = ${JSON.stringify(store.getState())}
+        </script>
         <script src="bundle.js"></script>
     </body>
 </html>`;
