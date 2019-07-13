4eaf2f77888c6ec23e22a4e066e252e4b237f315
hnrchrdl <hinrich@posteo.de>
2019-07-13 16:09:42 +0200

    8-57

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -4,6 +4,7 @@ import { StaticRouter } from "react-router-dom";
 import { renderRoutes } from "react-router-config";
 import { Provider } from "react-redux";
 import Routes from "../client/Routes";
+import serialize from 'serialize-javascript'
 
 export default (req, store) => {
   const content = renderToString(
@@ -21,7 +22,7 @@ export default (req, store) => {
     <body>
         <div id="root">${content}</div>
         <script>
-          window.INITIAL_STORE = ${JSON.stringify(store.getState())}
+          window.INITIAL_STORE = ${serialize(store.getState())}
         </script>
         <script src="bundle.js"></script>
     </body>
