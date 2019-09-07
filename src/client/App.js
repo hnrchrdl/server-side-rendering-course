d8f3d9b50897eaf052ab3093bda133880045a448
hnrchrdl <hinrich@posteo.de>
2019-09-07 14:37:47 +0200

    9-68

--- src/client/App.js
+++ src/client/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { renderRoutes } from "react-router-config";
+import Header from "./components/header";
 
 const App = ({ route }) => {
   return (
     <div>
-      <h1>I am a header</h1>
+      <Header />
       {renderRoutes(route.routes)}
     </div>
   );
