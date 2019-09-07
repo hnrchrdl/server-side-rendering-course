517a49dcec249c07964d8b2b562b8ea9be32c884
hnrchrdl <hinrich@posteo.de>
2019-09-07 14:45:03 +0200

    9-69

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import usersReducer from "../reducers/usersReducer";
+import authReducer from "./authReducer";
 
 export default combineReducers({
-  users: usersReducer
+  users: usersReducer,
+  auth: authReducer
 });
